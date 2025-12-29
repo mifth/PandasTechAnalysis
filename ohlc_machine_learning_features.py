44167c921b06bffa64ebbb8b7df36a5122eafcf5
mifth <paulgeraskin@gmail.com>
2025-12-29 14:55:11 +0100

    Handle inf and NaN values in candle analysis functions
    Added handling for infinite and NaN values in several functions to ensure cleaner output.

--- ohlc_machine_learning_features.py
+++ ohlc_machine_learning_features.py
@@ -8,6 +8,7 @@ import pandas as pd
 def candle_weakness(ohlc_high: pd.Series, ohlc_low: pd.Series, 
                     ohlc_open: pd.Series, ohlc_close: pd.Series) -> pd.Series:
     candle_weakness = ((ohlc_open - ohlc_close).abs() / (ohlc_high - ohlc_low))
+    candle_weakness = candle_weakness.replace([np.inf, -np.inf, np.nan], 0)
     return candle_weakness
 
 # Is the candle a green candle (close > open)
@@ -18,12 +19,14 @@ def is_green_candle(ohlc_open: pd.Series, ohlc_close: pd.Series) -> pd.Series:
 def candle_taller_than_prev_hl(ohlc_high: pd.Series, ohlc_low: pd.Series) -> pd.Series:
     prev = (ohlc_high - ohlc_low).shift(1)
     taller_pct = ((ohlc_high - ohlc_low) / prev) - 1
+    taller_pct = taller_pct.replace([np.inf, -np.inf, np.nan], 0)
     return taller_pct
 
 # tallness of current candle vs previous candle body (open-close)
 def candle_taller_than_prev_oc(ohlc_open: pd.Series, ohlc_close: pd.Series) -> pd.Series:
     prev = (ohlc_close - ohlc_open).shift(1).abs()
     taller_pct = ((ohlc_close - ohlc_open).abs() / prev) - 1
+    taller_pct = taller_pct.replace([np.inf, -np.inf, np.nan], 0)
     return taller_pct
 
 # Is the close diff from 0 to 1
@@ -31,30 +34,28 @@ def open_or_close_diff(high: pd.Series, low: pd.Series, open_or_close: pd.Series
     size = high - low
     with np.errstate(divide="ignore", invalid="ignore"):
         pct = (open_or_close - low) / size
-    return pct.fillna(0)  # or np.nan, depending on your pipeline
-
-# Is the open above the center point of the candle
-def is_open_above_center(ohlc_high: pd.Series, ohlc_low: pd.Series, ohlc_open: pd.Series) -> pd.Series:
-    center = candle_center_point(ohlc_high, ohlc_low)
-    return (ohlc_open > center).astype(int)
+    pct = pct.replace([np.inf, -np.inf, np.nan], 0)
+    return pct
 
 # Rolling min and max pct change from close
 def min_rolling_pct(ohlc_low: pd.Series, ohlc_close: pd.Series, window: int) -> pd.Series:
-    return (ohlc_low.rolling(window=window).min() / ohlc_close) - 1
+    min_rolling_pct = (ohlc_low.rolling(window=window).min() / ohlc_close) - 1
+    min_rolling_pct = min_rolling_pct.replace([np.inf, -np.inf, np.nan], 0)
+    return min_rolling_pct
 
 # Rolling max pct change from close
 def max_rolling_pct(ohlc_high: pd.Series, ohlc_close: pd.Series, window: int) -> pd.Series:
-    return (ohlc_high.rolling(window=window).max() / ohlc_close) - 1
-
-# Close price returns over given periods. periods=1 is a previous candle.
-def close_returns(ohlc_close: pd.Series, periods: int = 1) -> pd.Series:
-    return ohlc_close.pct_change(periods=periods)
+    max_rolling_pct = (ohlc_high.rolling(window=window).max() / ohlc_close) - 1
+    max_rolling_pct = max_rolling_pct.replace([np.inf, -np.inf, np.nan], 0)
+    return max_rolling_pct
 
 # Rolling range (high - low) relative to previous rolling average range
 def rolling_range(ohlc_high: pd.Series, ohlc_low: pd.Series, window: int) -> pd.Series:
     range_1 = (ohlc_high - ohlc_low)
     range_2 = range_1.shift(1).rolling(window=window).mean()
-    return range_1 / range_2
+    final_range = (range_1 / range_2) - 1
+    final_range = final_range.replace([np.inf, -np.inf, np.nan], 0)
+    return final_range
 
 # Is doji candle
 def is_doji(ohlc_open: pd.Series, ohlc_close: pd.Series, ohlc_high: pd.Series, ohlc_low: pd.Series) -> pd.Series:
